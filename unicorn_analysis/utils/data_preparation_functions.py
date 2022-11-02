d7f14a901d2988b415781cff7dad2f08599c3e0a
Andrés Vivanco <vivanco.sebastian.a@gmail.com>
2022-11-02 07:55:17 -0500

    🧐 Preparation of Data
    - First, I prepared the data for analysis. You can find that preparation in the notebook 1.0
    - I have also created the file data_preparation_functions.py where you can find some functions that I have used in the cleaning process.
    - In the end, after cleaning, I saved that data frame in a CSV file. This file is located in the processed data directory.
    
    Note: At this moment, in order to make further progress on the project, I have left the notebook without documentation. Later I will document all the code.

---
+++ unicorn_analysis/utils/data_preparation_functions.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def remove_foreign_characters(data):
+    values = []
+
+    for value in data:
+        without_foreign_characters = value[1: len(value)-1]
+        values.append(without_foreign_characters)
+    
+    return values
+
+
+
+def change_value(data, value):
+    values = []
+
+    for val in data:
+        if val == value:
+            values.append(np.nan)
+        else:
+            values.append(val)
+    
+    return values
+
+
+
+def million_to_billion(data, confirmation):
+    billion_values = []
+
+    for i in range(len(data)):
+        if confirmation[i][len(confirmation[i])-1] == 'M':
+            billion = data[i]/1000
+            billion_values.append(billion)
+        else:
+            billion_values.append(data[i])
+    
+    return billion_values
